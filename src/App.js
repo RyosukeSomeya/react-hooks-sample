d1185a57afd406731dad10a1bfa50f15f62720ac
RyosukeSomeya <rx.78.0618@gmail.com>
2021-10-28 15:48:51 +0900

    複数の状態を扱うコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  // useStateは配列を返す
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-
-  const increment2 = () => setCount((prevCount) => prevCount + 1);
-  const decrement2 = () => setCount((prevCount) => prevCount - 1);
-  const double = () => setCount((prevCount) => prevCount * 2);
-  const div3 = () =>
-    setCount((prevCount) => {
-      if (prevCount % 3 === 0) {
-        return prevCount / 3;
-      } else {
-        return prevCount;
-      }
-    });
-
-  const reset = () => setCount(0);
-
+  const rest = () => {
+    setName(props.name);
+    setPrice(props.price);
+  };
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>x2</button>
-        <button onClick={div3}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>
+        現在の{name}は、{price}円です
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={rest}>reset</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </>
   );
+};
+
+App.defaultProps = {
+  name: '',
+  price: 1000,
 };
 
 export default App;
