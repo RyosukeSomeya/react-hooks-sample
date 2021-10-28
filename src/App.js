2bf732d58e8787a1f518c3800f18f1b11d79f1d8
RyosukeSomeya <rx.78.0618@gmail.com>
2021-10-28 17:17:32 +0900

    implement useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = (props) => {
   const [state, setState] = useState(props);
-  const {name, price} = state;
+  const { name, price } = state;
+
+  // レンダリング後に呼ばれる
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate');
+  });
+  // 初回レンダリング後にだけ呼ばれる
+  useEffect(() => {
+    console.log('This is like componentDidMount');
+  }, []);
+
+  // nameが更新されたときだけレンダリング後に呼ばれる
+  useEffect(() => {
+    console.log('This callback is for name only.');
+  }, [name]);
+
+  const renderPeriod = () => {
+    console.log('renderPriod renders period.');
+    return '。';
+  };
 
   return (
     <>
       <p>
-        現在の{state.name}は、{price}円です
+        現在の{state.name}は、{price}円です{renderPeriod()}
       </p>
-      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
       <button onClick={() => setState(props)}>reset</button>
       <input
         type="text"
         value={name}
-        onChange={(e) => setState({...state, name: e.target.value})}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
       />
     </>
   );
-};;
+};
 
 App.defaultProps = {
   name: '',
