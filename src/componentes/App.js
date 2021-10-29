2eda29811a14e53a645875861d0dcf0bc53c0bce
RyosukeSomeya <rx.78.0618@gmail.com>
2021-10-29 13:06:45 +0900

    implement all events delete

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -23,6 +23,14 @@ const App = () => {
     setBody('');
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm('全てのイベントを本当に削除しますか？');
+    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' });
+  };
+
+  const unCreateable = title === '' || body === '';
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -45,10 +53,20 @@ const App = () => {
             onChange={(e) => setBody(e.target.value)}
           />
         </div>
-        <button className="btn btn-primary" onClick={addEvent}>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreateable}
+        >
           イベントを作成する
         </button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >
+          全てのイベントを削除する
+        </button>
       </form>
       <h4 className="mt-3">イベント一覧</h4>
       <table className="table table-hover">
